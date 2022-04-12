eaeafe6776bc27340823fbce1f10789534e81665
Remigiusz Wasiak <rw@MacBook-Pro-Remigiusz-2.local>
2022-04-12 21:59:07 +0200

    small changes

--- js/app.js
+++ js/app.js
@@ -5,24 +5,74 @@ const errorsTypes = {
   numberLength: "Phone number is incorrect!",
   email: "@ required!",
   check: "You must accept the rules!",
-}
+};
 
-// przechwycenie i zbudowanie globalErrorsValues niezależnie od ilości inputów
+// window.addEventListener("DOMContentLoaded", () => {
+//   const form = document.querySelector("form");
+//   const formElements = Array.from(form.elements);
+//   // inputs = document.querySelectorAll("input");
+
+//   formElements.forEach((el) => {
+//     el.onblur = validate;
+//     // el.oninvalid = onInvalid;
+//   });
+// });
+
+// function onInvalid(e) {
+//   console.log(e.target);
+// }
+
+// function validate(e) {
+//   const element = e.target;
+//   console.log("element", element.validity);
+//   const isValid = element.validity.valid;
+
+//   if (!isValid) {
+//     showError(element);
+//   } else {
+//     removeError(element);
+//   }
+// }
+
+// function showError(element) {
+//   console.log("element error", element.validity);
+//   if (element.validity.valueMissing) {
+//     const errorElement = element.parentElement.querySelector(".error-message");
+
+//     if (!errorElement) {
+//       createErrorElement(element, errorsTypes.empty);
+//     }
+//   }
+// }
+
+// function removeError(element) {
+//   const errorElement = element.parentElement.querySelector(".error-message");
+//   errorElement.remove();
+// }
+
+// function createErrorElement(element, error) {
+//   const p = document.createElement("p");
+//   p.textContent = error;
+//   p.classList.add("error-message");
+//   element.parentElement.insertBefore(p, element);
+// }
+
+// // przechwycenie i zbudowanie globalErrorsValues niezależnie od ilości inputów
 let globalErrorsValues = [];
-window.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('.form');
+window.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".form");
   const formElementsArray = [...form.elements];
 
   for (let i = 0; i < formElementsArray.length; i++) {
     globalErrorsValues.push({
       element: formElementsArray[i].name,
       listOfErrors: [],
-    })
+    });
   }
-  console.log(globalErrorsValues);
+  // console.log(globalErrorsValues);
 });
 
-// STRUKTURA TABLICY OBIEKTÓW globalErrorsValues :
+// // STRUKTURA TABLICY OBIEKTÓW globalErrorsValues :
 // let globalErrorsValues = [{
 //   element: "userName",
 //   listOfErrors: ["Input must be filled out!", "Incorrect type of data!"]
@@ -34,63 +84,78 @@ window.addEventListener('DOMContentLoaded', () => {
 //itd
 // ];
 
-
-// funkcja checkIfErrorExist ma za zadanie sprawdzić czy blur input posiada w globalErrorsValues dany typ błędu
+// // funkcja checkIfErrorExist ma za zadanie sprawdzić czy blur input posiada w globalErrorsValues dany typ błędu
 const checkIfErrorExist = (activeId, activeError) => {
   globalErrorsValues.forEach((formElement, index) => {
-    console.log(`sprawdzam czy podany w funkcji element ${activeId} === ${formElement.element}`);
+    // console.log(
+    //   `sprawdzam czy podany w funkcji element ${activeId} === ${formElement.element}`
+    // );
     if (formElement.element === activeId) {
-      console.log("element podany w funkcji jest równy sprawdzanemu elementowi");
+      // console.log(
+      //   "element podany w funkcji jest równy sprawdzanemu elementowi"
+      // );
       if (formElement.listOfErrors.includes(activeError)) {
-        console.log("zawiera activeError");
+        // console.log("zawiera activeError");
         return null;
       } else {
-        console.log("nie zawiera activeError");
-        formElement.listOfErrors.push(activeError)
+        // console.log("nie zawiera activeError");
+        formElement.listOfErrors.push(activeError);
       }
+    } else {
+      // console.log(
+      //   "element podany w funkcji nie jest równy sprawdzanemu elementowi"
+      // );
     }
-    else { console.log("element podany w funkcji nie jest równy sprawdzanemu elementowi"); }
-  })
-}
+  });
+};
 
 // chcialem walidację zrobić w jednej funkcji validation (bo nasłuchuje na blur cały formularz - miało być uniwersalnie niezależnie od ilości inputów ale jest problem z instrukcjami warunkowymi bo chciałem w obrębie jednej funkcji zrobić kilka razy if -> else. A else działa dla każdego innego warunku. Jest problem bo usuniecie błedu w jednym inpucie usuwa błedy w innym inpucie -przez else. Jak to zrobić?else if nie może być bo podaje konkretne errorsTypes.(np)empty)
-const validation = e => {
+const validation = (e) => {
   const activeElement = e.target;
-  const indexInGlobalErr = globalErrorsValues.findIndex(el => el.element === activeElement.id);
+  const indexInGlobalErr = globalErrorsValues.findIndex(
+    (el) => el.element === activeElement.id
+  );
   if (!activeElement.value) {
     vievError(activeElement, errorsTypes.empty, "empty", indexInGlobalErr);
     checkIfErrorExist(activeElement.id, errorsTypes.empty);
-  }
-  else {
-    globalErrorsValues[indexInGlobalErr].listOfErrors = [...globalErrorsValues[indexInGlobalErr].listOfErrors.filter(e => e !== errorsTypes.empty)];
-    hideError("empty");
+  } else if (activeElement.value) {
+    globalErrorsValues[indexInGlobalErr].listOfErrors = [
+      ...globalErrorsValues[indexInGlobalErr].listOfErrors.filter(
+        (e) => e !== errorsTypes.empty
+      ),
+    ];
+    console.log("globalErrorsValues", globalErrorsValues);
+    hideError("empty", activeElement);
   }
 
-  if (activeElement.type === "email" && !activeElement.value.includes('@')) {
+  if (activeElement.type === "email" && !activeElement.value.includes("@")) {
     vievError(activeElement, errorsTypes.email, "email", indexInGlobalErr);
     checkIfErrorExist(activeElement.id, errorsTypes.email);
-
+  } else if (
+    activeElement.type === "email" &&
+    activeElement.value.includes("@")
+  ) {
+    globalErrorsValues[indexInGlobalErr].listOfErrors = [
+      ...globalErrorsValues[indexInGlobalErr].listOfErrors.filter(
+        (e) => e !== errorsTypes.email
+      ),
+    ];
+    hideError("email", activeElement);
   }
-  else {
-    globalErrorsValues[indexInGlobalErr].listOfErrors = [...globalErrorsValues[indexInGlobalErr].listOfErrors.filter(e => e !== errorsTypes.email)];
-    hideError("email");
-  }
-
-}
+};
 // funkcja pokazująca czerwoną belke error w formularzu w przypadku błedu
 const vievError = (element, error, atr, index) => {
   if (!globalErrorsValues[index].listOfErrors.includes(error)) {
-    const p = document.createElement('p');
+    const p = document.createElement("p");
     p.textContent = error;
     p.classList.add("error-message");
     p.dataset.error = atr;
     element.parentElement.insertBefore(p, element);
   }
-}
-
+};
 
 // przechwycenie value inputów -> do wykorzystania przy walidacji on submit
-const addUser = e => {
+const addUser = (e) => {
   e.preventDefault();
   const values = {};
   for (let el of e.target) {
@@ -99,21 +164,30 @@ const addUser = e => {
       values[el.name] = el[value];
     }
   }
-}
+};
 
-hideError = (typeError) => {
-  document.querySelectorAll(`[data-error="${typeError}"]`).forEach(element => element.classList.add("d-none"));
-}
+const hideError = (typeError, element) => {
+  console.log("typeError", typeError);
+  console.log("element", element);
+  const errorElement = element.parentElement.querySelector(
+    `[data-error="${typeError}"]`
+  );
+  if (errorElement) {
+    errorElement.remove();
+  }
+};
 
-
-
-const deleteRow = e => {
+const deleteRow = (e) => {
   const rowToDelete = e.currentTarget;
   if (e.target.classList.contains("btn-delete")) {
-    rowToDelete.parentElement.removeChild(rowToDelete)
+    rowToDelete.parentElement.removeChild(rowToDelete);
   }
-}
+};
 
-document.querySelector('.form').addEventListener("submit", addUser);
-document.querySelector('.form').addEventListener("blur", validation, true);
-document.querySelectorAll('.new-user').forEach(row => row.addEventListener("click", deleteRow));
+document.querySelector(".form").addEventListener("submit", addUser);
+const form = document.querySelector(".form");
+const formElements = Array.from(form.elements);
+formElements.forEach((el) => el.addEventListener("blur", validation, true));
+document
+  .querySelectorAll(".new-user")
+  .forEach((row) => row.addEventListener("click", deleteRow));
